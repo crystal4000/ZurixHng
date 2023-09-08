d712aeb83a8f55c302cb91f6d69dd9173cd3d00d
Tania <38540885+crystal4000@users.noreply.github.com>
2023-09-08 17:51:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Slack Name
+  const slackNameElement = document.querySelector(
+    "[data-testid='slackUserName']"
+  );
+  // slackNameElement.textContent = "Slack Name: Amanda Fredrick";
+
+  // Current Day of the Week
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+  const currentDayOfWeekElement = document.querySelector(
+    "[data-testid='currentDayOfTheWeek']"
+  );
+  currentDayOfWeekElement.textContent =
+    "Current Day of the Week: " + currentDayOfWeek;
+
+  // Current UTC Time
+  const currentUTCTimeElement = document.querySelector(
+    "[data-testid='currentUTCTime']"
+  );
+  currentUTCTimeElement.textContent =
+    "Current UTC Time (in milliseconds): " + Date.now();
+
+  // Track
+  const trackElement = document.querySelector("[data-testid='myTrack']");
+  trackElement.textContent = "Track: Frontend";
+});
