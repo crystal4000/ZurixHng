7366795cd4862067fe36e0fe38d54c5c3f9ebacf
Tania <38540885+crystal4000@users.noreply.github.com>
2023-10-02 13:46:30 -0500

    Push

--- helpmeout-web/src/Components/Home.js
+++ helpmeout-web/src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Logo from "../assets/helpmeout.png";
+import Hero from "../assets/hero.svg";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Home = () => {
   return (
     <>
-      <header className="bg-info mt-3">
+      <header className="mt-3">
         <Navbar>
           <Container>
             <Navbar.Brand>
@@ -21,10 +22,10 @@ const Home = () => {
 
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mx-auto">
-                <a class="nav-link" href="/">
+                <a class="nav-link" href="#features">
                   Features
                 </a>
-                <a class="nav-link" href="/">
+                <a class="nav-link" href="#howitworks">
                   How It Works
                 </a>
               </Nav>
@@ -32,7 +33,7 @@ const Home = () => {
             <div className="toggle-main justify-content-end align-items-center">
               <a
                 href="/"
-                className="text-white text-decoration-none text-base me-3 mt-sm-2"
+                className="text-decoration-none text-base me-3 mt-sm-2"
               >
                 Get Started
               </a>
@@ -41,6 +42,29 @@ const Home = () => {
           </Container>
         </Navbar>
       </header>
+
+      <div className="hero ">
+        <div className="container">
+          <div className="row pt-5">
+            <div className="col-6 bg-info mx-auto mt-5 pt-5">
+              <h2>
+                Show Them <br /> Don't Just Tell
+              </h2>
+              <p>
+                Help your friends and loved ones by creating and sending videos
+                on how to get things done on a website.
+              </p>
+
+              <button className="mt-3 text-white btn btn-primary">
+                Install HelpMeOut <i class="ms-2 bi bi-arrow-right"></i>
+              </button>
+            </div>
+            <div className="col-6 bg-danger mx-auto">
+              <img src={Hero} alt="hero" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
