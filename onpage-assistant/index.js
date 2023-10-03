0ed1a8fa344f560bd18daf8e334968e06196f18d
Tania <38540885+crystal4000@users.noreply.github.com>
2023-10-03 12:01:29 -0500

    OnPage Assitant

---
+++ onpage-assistant/index.js
@@ -0,0 +1,48 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+window.addEventListener("load", function () {
+  const scene = new THREE.Scene();
+
+  // Create a simple cube
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const light = new THREE.PointLight(0xffffff, 1000);
+  light.position.set(2.5, 7.5, 15);
+  scene.add(light);
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0.8, 1.4, 1.0);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+    alpha: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.target.set(0, 1, 0);
+
+  var clock = new THREE.Clock();
+  function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+});
